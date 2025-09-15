301e5fd695b94fdba6a1e045876564bbb998b36b
anas0022 <anasfamilyman@gmail.com>
2025-09-15 22:56:46 +0530

    up

--- resources/views/Supperadmin/layout/sidebar.blade.php
+++ resources/views/Supperadmin/layout/sidebar.blade.php
@@ -32,7 +32,7 @@
     </div>
     <div class="scrollbar-sidebar ps ps--active-y">
         <div class="app-sidebar__inner">
-            <ul class="vertical-nav-menu">
+            <ul class="vertical-nav-menu" id="sidebaritems">
                 {{-- <li class="app-sidebar__heading">Menu</li> --}}
                 {{-- one side menu start --}}
                 <li class="{{ request()->is('superadmin/Create Menu/*') ? 'mm-active' : '' }}">
@@ -67,3 +67,78 @@
         </div>
     </div>
 </div>
+
+<script>
+$(document).ready(function(){
+    function isActive(path) {
+        return window.location.pathname === path ? 'mm-active' : '';
+    }
+alert(window.location.pathname )
+    window.loadMenu = function () {
+        $.ajax({
+            url: '/sidebar/menu',
+            method: 'GET',
+            success: function (response) {
+           // <-- clear before appending
+
+                let html = "";
+
+                response.forEach(function(module) {
+                    html += `
+                        <li class="${isActive('/SupperAdmin')}">
+                            <a href="#">
+                                <i class="metismenu-icon fa ${module.icon || 'fa-box'}"></i>
+                                ${module.modulename}
+                                <i class="metismenu-state-icon fa fa-caret-down"></i>
+                            </a>
+                            <ul>
+                    `;
+
+                    module.menu.forEach(function(menu) {
+                        html += `
+                            <li>
+                                <a href="${menu.route}" class="${isActive(menu.route)}">
+                                    <i class="metismenu-icon"></i>${menu.Menuname}
+                                </a>
+                        `;
+
+                        if (menu.submenus && menu.submenus.length > 0) {
+                            html += `<ul>`;
+                            menu.submenus.forEach(function(sub) {
+                                html += `
+                                    <li>
+                                        <a href="${sub.sub_route}" class="${isActive(sub.sub_route)}">
+                                            <i class="metismenu-icon"></i>${sub.menuname}
+                                        </a>
+                                    </li>
+                                `;
+                            });
+                            html += `</ul>`;
+                        }
+
+                        html += `</li>`;
+                    });
+
+                    html += `</ul></li>`;
+                });
+
+                $('#sidebaritems').append(html);
+
+                if ($('#sidebaritems').data('metismenu')) {
+                    $('#sidebaritems').metisMenu('dispose');
+                }
+                $('#sidebaritems').metisMenu();
+            },
+            error: function (xhr, status, error) {
+                console.error('loadMenu AJAX error:', status, error, xhr.responseText);
+            }
+        });
+    };
+
+    loadMenu();
+    // setInterval(loadMenu, 5000); // optional
+});
+
+</script>
+
+
